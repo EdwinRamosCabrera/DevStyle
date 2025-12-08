59ed88829857831779d24492c7476176ea5ab649
Edwin Ramos <edwin_ramos2@usmp.pe>
2025-12-07 22:51:59 -0500

    Addition of the Jazzmin package

--- app_store/models.py
+++ app_store/models.py
@@ -7,6 +7,10 @@ class Category(models.Model):
     slug = models.SlugField(max_length=100)
     description = models.TextField()
 
+    class Meta:
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
+
     def __str__(self):
         return self.name
 
@@ -20,11 +24,16 @@ class Product(models.Model):
     category = models.ForeignKey('Category', on_delete=models.PROTECT)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     description = models.TextField()
-    image = models.ImageField(upload_to='products/')
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
     stock = models.IntegerField(default=0)
     is_available = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Producto'
+        verbose_name_plural = 'Productos'
+        ordering = ('created_at',)
 
     def __str__(self):
         return self.name
